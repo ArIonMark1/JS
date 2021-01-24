cbf50e01e4965ef70550df3de11a6f835a4a006c
ArIonMark1 <65442269+ArIonMark1@users.noreply.github.com>
2021-01-24 20:15:08 +0100

    Update scriptlesson4.js

--- scriptlesson4.js
+++ scriptlesson4.js
@@ -26,5 +26,46 @@ const numbObj = num => {
 let userNumb = 72;
 console.log(numbObj(userNumb));
 
+// 2.
+
+let shop_2 = [
+    {id: 'obj1', type: 'some shit', name: 'pipe', price: '500'},
+    {id: 'obj2', type: 'house', name: 'dor', price: '250'},
+    {id: 'obj3', type: 'tourism', name: 'skiing', price: '400'},
+    {id: 'obj4', type: 'kitchen', name: 'dough rocking hair', price: '150'},
+    {id: 'obj5', type: 'car', name: 'battery', price: '250'},
+];
+
+let basket_2 = [];
+
+function counter(showcase) {
+
+    // если товар в магазине - предложить покупателю
+    for (let product in showcase) {
+        alert(` ${showcase[product]['name']} : ${showcase[product]['price']} $`);
+
+        let userAns = prompt("If you like this product please press '+', if not - '-': ")
+        // если ползовател подтвердит: +
+        // добавить в корзину
+        if (userAns === '+') basket_2.push(showcase[product]);
+        // если ползовател: -
+        //пропустить товар
+        else continue
+
+    };
+
+    // когда товар весь был предложен, закончить вывод и начать подщет стоимости
+    //console.log(basket_2[0].price);
+
+    let totalSum = basket_2.reduce((total, nextPrice) => parseInt(total) + parseInt(nextPrice.price), 0);
+    
+    // вывод имени всех товаров в корзине и их стоимости 
+    for (i in basket_2) {
+        console.log(`${basket_2[i]['name']}:  ${basket_2[i]['price']}`);
+    }
+    return totalSum;
+    
+}
+console.log(counter(shop_2));
 
 
